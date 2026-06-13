7f79fc9e4e61532739761a995cf2c8126b77e1a4
madhaviporte <madhaviporte2004@gmail.com>
2026-06-13 21:17:56 +0530

    dom

---
+++ Dom-4/script.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector("button")
+const div = document.querySelector("div")
+const main = document.querySelector("main")
+const body = document.querySelector("body")
+
+btn.addEventListener("click", (events) => {
+    console.log("button triggerd");
+})
+div.addEventListener("click", (events) => {
+    console.log("button triggerd");
+})
+main.addEventListener("click", (events) => {
+    console.log("button triggerd");
+})
+body.addEventListener("click", (events) => {
+    console.log("button triggerd");
+})
