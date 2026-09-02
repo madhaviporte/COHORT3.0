d751a8d706bffe1892dc308cf196a49b389a747c
madhaviporte <madhaviporte2004@gmail.com>
2026-09-02 14:53:45 +0530

    backend

--- Backend/Day-2/server.js
+++ Backend/Day-2/server.js
@@ -1,12 +1,27 @@
-const express = require('express')
+// let http = require('http');
+// let server = http.createServer((req,res) =>{
+//     if(req.url === '/user'){
+//         res.end('me users me hun');
+//     }
+//     if(req.url === '/product'){
+//         res.end('me product me hun');
+//     }
+//     if(req.url === '/carts'){
+//         res.end('me  me hun');
+//     }
+// })
+// server.listen(3000, () => {
+//     console.log("server is running on port 3000");
+// })
+
+let express = require('express');
 
 const app = express();
 
-app.get('/', (req,res) =>{
-    res.send('ok got it')
+app.get('/' ,(req,res) =>{
+    res.send('ohk got it')
 })
-
-app.listen(3000, () =>{
-    console.log('server is running on port 3000');
-    
+let port = 3000
+app.listen(3000, ()=>{
+    console.log(`server is running on ${port}`);    
 })
