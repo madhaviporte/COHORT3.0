0066b0748627bce58e90fe624699117c13d9d263
madhaviporte <madhaviporte2004@gmail.com>
2026-09-05 18:29:10 +0530

    Day-3

--- Backend/Day-3/server.js
+++ Backend/Day-3/server.js
@@ -2,19 +2,34 @@ const express = require("express");
 
 const app = express();
 
+//middleware for accepting json data
+app.use(express.json())
 let port = 3000;
 
-let users = [{
-    name: "pol",
-    age:89,
-    }
-]
+let users = []
 
+//Create
+app.post('/create',(req,res)=>{
+    let body = req.body;
+
+    users.push(body);
+
+    res.send("user saved successfully");
+})
+
+//get-Read
 app.get('/', (req,res) => {
 res.send(users);
 })
 
+app.delete('/delete/:id',(req,res) =>{
+ let {id} = req.params;
+
+let userData = users.filter((val) => val.id !==id);
+users = userData;
+res.send("User deleted successfully")
+})
+
 app.listen(port, () =>{
     console.log(`Server is ruunning on port ${port}`);
-    
 })
