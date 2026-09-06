051ef1429b5ac097e79136b75c089c797f3c5cdf
madhaviporte <madhaviporte2004@gmail.com>
2026-09-06 19:35:38 +0530

    remove somthing

--- Backend/Day-5/src/config/db.js
+++ Backend/Day-5/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDb = async () =>{
 try {
-   await mongoose.connect("mongodb+srv://madhaviporte2004:varma2004@cohort-cluster.sqxhujk.mongodb.net/");
+   await mongoose.connect("");
 console.log("mongoDB connected");
 } catch (error) {
     console.log('error while connecting db', error);
